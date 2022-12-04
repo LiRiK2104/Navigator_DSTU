860096860f0e4e27a220b3872fda7a1f64867614
Kirill Sharikov <sarikovk38@gmail.com>
2022-12-04 22:14:53 +0300

    adaptate 1stCampus scene

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -4,12 +4,14 @@ public class Follower : MonoBehaviour
 {
     [SerializeField] private GameObject _target;
     [SerializeField] private bool _shouldYRotate;
-
+    
+    private Quaternion _targetStartRotation;
     private Vector3 _differentPosition;
     private Quaternion _differentRotation;
 
     private void Start()
     {
+        _targetStartRotation = _target.transform.rotation;
         _differentPosition = transform.position - _target.transform.position;
 
         if (_shouldYRotate)
@@ -18,12 +20,17 @@ public class Follower : MonoBehaviour
 
     private void Update()
     {
-        transform.position = _target.transform.position + _differentPosition;
-
         if (_shouldYRotate)
         {
+            var rotatedDifferenceVector = (_target.transform.rotation * Quaternion.Inverse(_targetStartRotation)) * _differentPosition;
+            transform.position = _target.transform.position + rotatedDifferenceVector;
+            
             var targetRotation = _differentRotation.eulerAngles + new Vector3(0, _target.transform.rotation.eulerAngles.y, 0);
             transform.rotation = Quaternion.Euler(targetRotation);
+        }
+        else
+        {
+            transform.position = _target.transform.position + _differentPosition;    
         }
     }
 }
