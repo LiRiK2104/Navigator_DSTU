9c8b90b71390c22963e81849270e99db714c7b8b
Kirill Sharikov <sarikovk38@gmail.com>
2023-03-31 18:45:46 +0300

    import GraphWay

---
+++ Assets/Plugins/ZenythStudios/Graphway/Assets/Scripts/GwJob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class GwJob
+{
+    public Vector3 origin;
+    public Vector3 targetPosition;
+    public Action<GwWaypoint[]> callback;
+    public bool clampToEndNode;
+    public bool debugMode;
+    public Vector3[] availableOrigins;
+
+    public GwJob(Vector3 origin, Vector3 targetPosition, Action<GwWaypoint[]> callback, bool clampToEndNode, bool debugMode)
+    {
+        this.origin = origin;
+        this.targetPosition = targetPosition;
+        this.callback = callback;
+        this.clampToEndNode = clampToEndNode;
+        this.debugMode = debugMode;
+    }
+    
+    public GwJob(Vector3 origin, Vector3 targetPosition, Action<GwWaypoint[]> callback, bool clampToEndNode, bool debugMode, Vector3[] availableOrigins) : this(origin, targetPosition, callback, clampToEndNode, debugMode)
+    {
+        this.availableOrigins = availableOrigins;
+    }
+}
