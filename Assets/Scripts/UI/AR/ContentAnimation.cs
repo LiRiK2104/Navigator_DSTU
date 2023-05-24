9fed81477a09be4af521362c9fd4c4ee726a4dcb
Kirill Sharikov <sarikovk38@gmail.com>
2023-05-24 19:27:47 +0300

    Fix AR Animation

--- Assets/Scripts/UI/AR/ContentAnimation.cs
+++ Assets/Scripts/UI/AR/ContentAnimation.cs
@@ -2,18 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
-using DG.Tweening.Core;
-using DG.Tweening.Plugins;
-using DG.Tweening.Plugins.Core.PathCore;
-using DG.Tweening.Plugins.Options;
-using Helpers;
 using UnityEngine;
 
 namespace UI.AR
 {
     public class ContentAnimation : MonoBehaviour
     {
-        [SerializeField] private MovableMask _phoneMask;
+        [SerializeField] private Transform _phone;
         [SerializeField] private List<Transform> _phonePathPoints = new List<Transform>();
 
         private Tween _tween;
@@ -31,7 +26,7 @@ namespace UI.AR
             var pathType = PathType.CatmullRom;
             var path = _phonePathPoints.Select(point => point.position).ToArray();
             
-            _tween = _phoneMask.DOPath(path, duration, pathType);
+            _tween = _phone.DOPath(path, duration, pathType);
             _tween.SetEase(Ease.Linear);
             _tween.SetLoops(-1);
         }
